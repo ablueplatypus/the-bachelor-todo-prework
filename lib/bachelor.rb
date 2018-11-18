6eb8130db6b6cf62ad0619d59f2cc916cfc77641
Matt Doering <mattdoering86@gmail.com>
2018-11-18 23:37:55 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -11,8 +11,14 @@ def get_first_name_of_season_winner(data, season)
 end
 
 def get_contestant_name(data, occupation)
-
-
+data.each do |seaons, array|
+  array.each do |contestant|
+    contestant.each do |key, value|
+      if value == occupation
+        return contestant["name"]
+      end
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
