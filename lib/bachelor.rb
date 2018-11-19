d98faefeaa09d407ceb560c54d135e53344585b0
Matt Doering <mattdoering86@gmail.com>
2018-11-19 00:18:33 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -53,6 +53,9 @@ def get_average_age_for_season(data, season)
   data[season].each do |contestants|
       contestants.each do |key, value|
         if key == "age"
-
-
+          ages.push(value.to_i)
+        end
+      end
+    end
+    binding.pry
 end
