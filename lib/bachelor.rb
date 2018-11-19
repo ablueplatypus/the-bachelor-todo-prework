39c7e289fa23d6bdf941d4782127c161096f57f4
Matt Doering <mattdoering86@gmail.com>
2018-11-19 00:21:13 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -57,5 +57,9 @@ def get_average_age_for_season(data, season)
         end
       end
     end
-
+    average = 0
+    ages.collect do |numbers|
+      average = numbers + numbers / ages.length
+    end
+average
 end
